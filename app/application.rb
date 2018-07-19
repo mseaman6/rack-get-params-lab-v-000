2050ea79579ac7bf6c3ebfa7ec440f8f998e67fc
Monica Seaman <monica.l.seaman@gmail.com>
2018-07-19 13:29:15 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,11 +8,13 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      @@items.each do |item|dfjs
+      @@items.each do |item|
         resp.write "#{item}\n"
       end
     elsif req.path.match(/cart/)
-      
+      @@cart.each do |cart_item|
+        resp.write "#{cart_item}\n"
+      end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
